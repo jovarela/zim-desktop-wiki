1d0487d3b846b4f211ed0b096bf32b95eff53fd4
Jaap Karssenberg <jaap.karssenberg@gmail.com>
2026-06-17 20:52:08 +0200

    fixup windows build for fonts

--- windows/install-msys2-requirements.sh
+++ windows/install-msys2-requirements.sh
@@ -4,15 +4,17 @@
 # Common between github build actions and build.sh script
 
 # Skip font cache update
-export MSYS2_FC_CACHE_SKIP=1
+#export MSYS2_FC_CACHE_SKIP=1
 
 # Install build dependencies
 pacman --noconfirm -S --needed \
     git \
     make \
     mingw-w64-x86_64-gtk3 \
+    mingw-w64-x86_64-cairo \
     mingw-w64-x86_64-python \
     mingw-w64-x86_64-python-gobject \
+    mingw-w64-x86_64-python-cairo \
     mingw-w64-x86_64-python-setuptools \
     mingw-w64-x86_64-gobject-introspection \
     mingw-w64-x86_64-gtksourceview3 \
